9ce8be2a7c53f9bf40727bec5f0591f2aa8445bd
chloebouerii <bouerichloe@gmail.com>
2024-10-01 16:16:06 +0300

    Create footer page

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Footer from "./components/organisms/Footer";
 import reportWebVitals from "./reportWebVitals";
 import "./styles.css";
 
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
