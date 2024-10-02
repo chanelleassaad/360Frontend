89d0fda921b9fbafc0af53e1b935ef97074ae4f6
chanelleassaad <assaadchanelle@gmail.com>
2024-10-02 16:38:59 +0300

    Fix contact us styling

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -1,6 +1,6 @@
 import { AtSymbolIcon, PhoneIcon } from "@heroicons/react/24/outline";
 import { useRef } from "react";
-import image from "../assets/360logo.png"
+import image from "../assets/360logo.png";
 
 const features = [
   {
@@ -25,8 +25,7 @@ const getIconComponent = (iconName: string) => {
 };
 
 export default function ContactUs() {
-
-  // form in order for the user to contact me 
+  // form in order for the user to contact me
   const formRef = useRef<HTMLFormElement | null>(null);
 
   const handleReset = () => {
@@ -37,113 +36,114 @@ export default function ContactUs() {
 
   return (
     <div className="bg-gradient-to-b from-transparent to-white">
-    <div className="flex border-section mx-auto grid  grid-cols-1 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-      <div className="flex-1 lg:pr-8 lg:pt-4">
-        <div className="lg:max-w-lg">
-          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Contact Us
-          </p>
-          <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none display-center">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative pl-9">
-                <dt className="flex font-semibold text-white space-x-20">
-                  {getIconComponent(feature.icon)}
-                  {feature.name}
-                </dt>
-              </div>
-            ))}
-          </dl>
+      <div className="flex border-section mx-auto grid  grid-cols-1 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none md:grid-cols-2">
+        <div className="flex-1 lg:pr-8 lg:pt-4">
+          <div className="lg:max-w-lg">
+            <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Contact Us
+            </p>
+            <dl className="mt-10 max-w-xl space-y-8 text-base leading-7 lg:max-w-none display-center pl-2 p-10">
+              {features.map((feature) => (
+                <div key={feature.name} className="relative pl-9">
+                  <dt className="flex justify-between font-semibold text-white">
+                    {getIconComponent(feature.icon)}
+                    {feature.name}
+                  </dt>
+                </div>
+              ))}
+            </dl>
+          </div>
+          <div className="flex justify-center">
+            <img
+              className="pt-10 h-15 hidden md:block"
+              src={image}
+              alt=""
+            ></img>
+          </div>
         </div>
-        <img className="pt-10 h-15 hidden sm:block " src={image}></img>
-      </div>
 
-      {/* For future enhancement, this would actually send me a message and make sure that no input form is empty as well as have a small 'Sent' popup */}
-      <form className="flex-1 mt-4 mx-auto max-w-xl bg-white p-6 rounded-md mb-6">
-        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-          <div>
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-              />
+        <form className="flex-1 mt-4 mx-auto max-w-xl bg-white p-6 rounded-md mb-6">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                First name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Last name
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-grey-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="message"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Message
+              </label>
+              <div className="mt-2.5">
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={4}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
+                  defaultValue={""}
+                />
+              </div>
             </div>
           </div>
-          <div>
-            <label
-              htmlFor="last-name"
-              className="block text-sm font-semibold leading-6 text-gray-900"
+          <div className="mt-10">
+            <button
+              type="submit"
+              onClick={handleReset}
+              className="block w-full rounded-md bg-gray-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
             >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-grey-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+              Submit
+            </button>
           </div>
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>            
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-gray-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="mt-10">
-          <button
-            type="submit"
-            onClick={handleReset}
-            className="block w-full rounded-md bg-gray-700 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
     </div>
   );
 }
-
-
-// make the phone and email be smaller for mobile app 
-// make the contact us page always in the same page 
