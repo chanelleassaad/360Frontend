e17fd388b1b610f3d3ed19876826b86e879fdb8f
chanelleassaad <assaadchanelle@gmail.com>
2024-10-29 14:34:15 +0200

    Finalize main view

--- src/pages/MainView.tsx
+++ src/pages/MainView.tsx
@@ -1,17 +1,18 @@
 import logo from "../assets/360logo.png";
+import background from "../assets/main-background.jpeg";
 
 export default function MainView() {
   return (
-    <div className="relative isolate overflow-hidden bg-gray-900 py-5">
+    <div className="relative isolate overflow-hidden  bg-gray-900 py-5">
       {/* Background Image */}
       <img
         alt=""
-        src="https://surteeshire.com/wp-content/uploads/2021/04/corporate-event-production.png"
+        src={background}
         className="absolute inset-0 -z-10 h-full w-full object-cover object-center"
       />
 
       {/* Dark Overlay */}
-      <div className="absolute inset-0 bg-black opacity-40 -z-10"></div>
+      <div className="absolute inset-0 bg-black opacity-70 -z-10"></div>
 
       {/* Shadowings */}
       <div
@@ -41,12 +42,12 @@ export default function MainView() {
 
       {/* Data */}
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="flex flex-col lg:flex-row justify-between items-center">
+        <div className="flex flex-col md:flex-row lg:flex-row justify-between items-center">
           <div className="mx-auto max-w-2xl lg:mx-0 lg:w-1/2">
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+            <h2 className="font-bold tracking-tight text-white md:text-5xl text-4xl">
               360 Productions
             </h2>
-            <h3 className="text-3xl tracking-tight text-white sm:text-3xl">
+            <h3 className="tracking-tight text-white md:text-3xl text-2xl">
               Saudi Arabia
             </h3>
             <p className="mt-6 text-lg leading-8 text-gray-300">
@@ -56,7 +57,7 @@ export default function MainView() {
             </p>
           </div>
           {/* Logo section */}
-          <div className="lg:w-1/2 flex justify-center lg:justify-end">
+          <div className="w-1/2 flex justify-center ">
             <img
               alt="360 Logo"
               src={logo}
